e8f939e2fddc084e6b42d8b06b5026d00cfc0175
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-02 15:32:38 +0100

    add project

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>React at ALX!</h1>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!</p>
         </div>
@@ -9,3 +9,4 @@ function WelcomeMessage() {
 }
 
 export default WelcomeMessage;
+
