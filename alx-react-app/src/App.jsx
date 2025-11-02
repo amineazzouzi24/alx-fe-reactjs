c80efde836477f7ba8b7aab03b129518e7e5cf56
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-02 16:07:21 +0100

    add project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+  );
+}
+
+export default App;
+
+import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
