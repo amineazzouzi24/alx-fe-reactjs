81bb91b45a4c8b6136aee47405f7e57a45b825b1
MOHAMED AMINE AZZOUZI <93558172+amineazzouzi24@users.noreply.github.com>
2025-11-10 02:06:09 +0100

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p>&copy; 2025 My React App</p>
-    </footer>
-  );
-}
-
-export default Footer;
