90ff880ce71ba9e5d83e735d3effbb7ee519adca
MOHAMED AMINE AZZOUZI <93558172+amineazzouzi24@users.noreply.github.com>
2025-11-10 06:04:10 +0100

    App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,13 @@
-import Counter from './components/Counter';
+import { UserContext } from './components/UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' };
+
   return (
-    <div>
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
