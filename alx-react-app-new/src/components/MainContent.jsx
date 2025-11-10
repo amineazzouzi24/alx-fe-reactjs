0529e59a567619d9762c1cb3eaa5eee6be561198
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-10 03:57:17 +0100

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#eef', padding: '10px', textAlign: 'center' }}>
+      <h2>My Travel Bucket List</h2>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
