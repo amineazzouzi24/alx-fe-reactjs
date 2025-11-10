df9814248f391449ef4cef7bacfcefead58b0dda
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-10 01:20:04 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+   <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+  <p>© 2025 My React App</p>
+</footer>
+
+  );
+}
+
+export default Footer;
