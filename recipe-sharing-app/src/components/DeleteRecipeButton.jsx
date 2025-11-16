be15b6e887c6f90783f25849130fd1e8f19b8baf
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-16 16:52:19 +0100

    Initial commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,39 @@
+import { useNavigate } from "react-router-dom";
+
 import { useRecipeStore } from "./recipeStore";
 
-function DeleteRecipeButton({ recipeId }) {
+
+
+const DeleteRecipeButton = ({ id }) => {
+
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
+    const navigate = useNavigate();
+
+
+
+    const handleDelete = () => {
+
+        deleteRecipe(id);
+
+        navigate("/");
+
+    };
+
+
+
     return (
-        <button onClick={() => deleteRecipe(recipeId)}>
+
+        <button onClick={handleDelete}>
+
             Delete Recipe
+
         </button>
+
     );
-}
+
+};
+
+
 
 export default DeleteRecipeButton;
