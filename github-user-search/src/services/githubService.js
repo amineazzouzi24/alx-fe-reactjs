a8bacd40140d9592a31cf36d1c12c5049fcf1f39
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-23 10:38:41 +0100

    Initial commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -8,3 +8,12 @@ export const fetchUserData = async (username) => {
     throw error;
   }
 };
+export const fetchUsersAdvanced = async ({ username, location, minRepos }) => {
+  let query = '';
+  if (username) query += `${username}`;
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return response.data.items;
+};
