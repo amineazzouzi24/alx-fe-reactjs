adf032cab7bfb958ec16e437622c0742d4634035
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-23 10:13:58 +0100

    Initial commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,10 @@
-import Search from "./components/Search";
+import React from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="min-h-screen p-10">
-      <h1 className="text-3xl font-bold mb-6 text-center">GitHub User Search</h1>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>GitHub User Search</h1>
       <Search />
     </div>
   );
