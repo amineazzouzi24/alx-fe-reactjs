fd38d2a54afab11f5fa66fceb3be01a78aadcb20
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-11-30 18:49:54 +0100

    Initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// src/App.jsx
+import React from 'react';
+import UserProfile from './components/UserProfile';
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+export default App;
 
 function App() {
   const [count, setCount] = useState(0)
