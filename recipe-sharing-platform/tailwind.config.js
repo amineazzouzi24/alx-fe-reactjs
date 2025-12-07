cf50c585bbfb419919d5c70a51425d7244484632
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-12-07 02:46:42 +0100

    Initial commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: [],
-  theme: {
+  content: [
+    "./public/index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: false,
     extend: {},
   },
   plugins: [],
-}
+};
