785e21bc6829af52796302a46f7590dd0b967697
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-12-14 20:14:32 +0100

    Initial commit

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -13,31 +13,23 @@ describe('TodoList Component', () => {
 
   test('adds a new todo', () => {
     render(<TodoList />);
-
-    // Get input and button
     const input = screen.getByPlaceholderText('Add new todo');
     const addButton = screen.getByText('Add');
 
-    // Simulate user typing and adding a new todo
     fireEvent.change(input, { target: { value: 'New Todo' } });
     fireEvent.click(addButton);
 
-    // Assert that new todo appears in the list
     expect(screen.getByText('New Todo')).toBeInTheDocument();
   });
 
   test('toggles a todo', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
-
-    // Initially not completed
     expect(todo).toHaveStyle('text-decoration: none');
 
-    // Toggle completion
     fireEvent.click(todo);
     expect(todo).toHaveStyle('text-decoration: line-through');
 
-    // Toggle back to incomplete
     fireEvent.click(todo);
     expect(todo).toHaveStyle('text-decoration: none');
   });
@@ -45,14 +37,9 @@ describe('TodoList Component', () => {
   test('deletes a todo', () => {
     render(<TodoList />);
     const deleteButtons = screen.getAllByText('Delete');
-
-    // Delete first todo
     fireEvent.click(deleteButtons[0]);
 
-    // The deleted todo should no longer exist
     expect(screen.queryByText('Learn React')).toBeNull();
-
-    // The second todo should still exist
     expect(screen.getByText('Write Todo App')).toBeInTheDocument();
   });
 });
