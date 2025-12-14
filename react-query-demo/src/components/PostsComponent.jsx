7a697bdf39761898f2756b71d80bb62a6eda92bb
mohamed amine azzouzi <amineazzouzi202298@gmail.com>
2025-12-14 11:29:36 +0100

    Initial commit

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -19,7 +19,16 @@ const PostsComponent = () => {
     isError,
     error,
     refetch,
-  } = useQuery("posts", fetchPosts);
+  } = useQuery(
+    "posts",
+    fetchPosts,
+    {
+      cacheTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60,     // 1 minute
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    }
+  );
 
   if (isLoading) {
     return <p>Loading posts...</p>;
